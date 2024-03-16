f6216203b2bfca6018a53c8f6b8c489380a748e3
lolay92 <laleyecheryf@gmail.com>
2024-03-16 21:38:37 +0100

    changed print message and recompile the main.cpp

--- executables/main.cpp
+++ executables/main.cpp
@@ -1,6 +1,6 @@
 #include "../cmkproject/utils/utils.h"
 
 int main(int argc, char** argv) {
-    cmkproject::utils::Print("Hey I am really good a cmaking!!!"); 
+    cmkproject::utils::Print("CMake template!"); 
     return 0; 
 }
